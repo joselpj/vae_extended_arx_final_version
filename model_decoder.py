216a5153bd0dc56562caa2f391cdecf1a792069b
joselpj <joselu.pjaramillo@gmail.com>
2021-11-11 02:28:38 -0500

    decoder_analysis.py added to test just decoder part of vaearx. the results are very well for all the systems

--- model_decoder.py
+++ model_decoder.py
@@ -4,10 +4,10 @@ import torch.nn.functional as F
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_dim, output_dim, layer_dim):
+    def __init__(self, input_dim, output_dim, layer_dim, latent_dim):
         
         super(Decoder, self).__init__()
-        self.dec1 = nn.Linear(input_dim+2, layer_dim)
+        self.dec1 = nn.Linear(input_dim+latent_dim, layer_dim)
         self.dec2 = nn.Linear(layer_dim, layer_dim)
         self.out = nn.Linear(layer_dim, output_dim)
 
